0efed79ee57268e88770b7c47e973066688e210c
faraz-rahimi <fwebstack@gmail.com>
2026-07-09 19:00:00 +0330

    add delete all button to script.js and it will be saved after refresh the page
    you can delete all and all changes will be saved.

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("input");
 const list = document.getElementById("list");
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+const savedTodos = localStorage.getItem("todos");
+
+if (savedTodos) {
+  list.innerHTML = savedTodos;
+  attachDeleteEvents();
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", list.innerHTML);
+}
+function attachDeleteEvents() {
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.parentElement.remove();
+      saveTodos();
+    });
+  });
+}
+deleteAllBtn.addEventListener("click", () => {
+  list.innerHTML = "";
+  saveTodos();
+});
 btn.addEventListener("click", () => {
-  text = input.value;
+  const text = input.value;
   if (!text) return;
   const li = document.createElement("li");
   li.classList.add("todo-item");
@@ -13,12 +38,16 @@ btn.addEventListener("click", () => {
   deleteBtn.classList.add("delete-btn");
   deleteBtn.addEventListener("click", () => {
     li.remove();
+    saveTodos();
   });
   li.textContent = text;
   deleteBtn.textContent = "delete";
   li.appendChild(deleteBtn);
+  saveTodos();
   li.prepend(checkbox);
+  saveTodos();
   list.appendChild(li);
+  saveTodos();
   input.value = "";
 });
 input.addEventListener("keydown", function (event) {
